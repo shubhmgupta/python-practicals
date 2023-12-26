f2188a351b7178f299b74cb97b5a4d747263905b
Shubham Gupta <shubhamguptacoder@gmail.com>
2023-12-26 10:59:22 +0530

    Question 12 D completed

---
+++ question12d.py
@@ -0,0 +1,3 @@
+# Consider a tuple t1=(1, 2, 5, 7, 9, 2, 4, 6, 8, 10). Return maximum and minimum value from this tuple 
+t1=(1, 2, 5, 7, 9, 2, 4, 6, 8, 10)
+print(f"Minimum : {min(t1)}, Maximum : {max(t1)}")
